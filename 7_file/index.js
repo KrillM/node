c5c4212819e3029f4922012fd905d96bd1d5a22a
KrillM <mks160615@gmail.com>
2023-10-21 15:25:38 +0900

    show images

--- 7_file/index.js
+++ 7_file/index.js
@@ -4,6 +4,9 @@ const multer = require("multer");
 const path = require("path"); // 파일 경로를 받았을 때, 그 조작을 도와준다. ex. 확장자를 가져오거나 파일이름을 추출한다.
 const port=9000;
 
+// 업로드한 사진을 보는 방법
+app.use("/files", express.static(__dirname+"/files"))
+
 // upload 함수는 multer를 통해 미들웨어로 활용이 가능
 const upload=multer({
     dest: "files/"
@@ -31,9 +34,8 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
 app.get("/", function(req, res){
-    res.render("index.ejs");
+    res.render("index1.ejs");
 })
-
 
 // file middleware. single() {req.file(), upload only one file}, array(), fields() {req.files(), upload many files}
 // the file, which `name` is `userfile`, save a file by multer's settings and create 'req.file' object and send to next function.
@@ -41,7 +43,23 @@ app.get("/", function(req, res){
 app.post("/upload", uploadDetails.single("userfile"), function(req, res){
     console.log("file: ", req.file); // file uploaded through here
     console.log("body: ", req.body); // rest
-    res.send("file uploaded!");
+    // res.send("file uploaded!");
+    res.render("result.ejs",{
+        src: req.file.path,
+        title: req.body.title
+    });
+})
+
+// 하나의 name으로 여러 개의 파일을 받는 방법
+app.post("/uploads1", uploadDetails.array("userfile"), function(req, res){
+    console.log("files: ", req.files); // file uploaded through here
+    res.send("files #1 uploaded!");
+})
+
+// 여러 개의 input을 사용하는 경우
+app.post("/uploads2", uploadDetails.fields([{name: "userfile1"}, {name: "userfile2"}]), function(req, res){
+    console.log("files: ", req.files); // file uploaded through here
+    res.send("files #2 uploaded!");
 })
 
 app.listen(port, function(){
