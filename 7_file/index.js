3eb894302a0fa2b297046235a96e967f63f0bab0
KrillM <mks160615@gmail.com>
2023-10-21 14:35:18 +0900

    detail upload file

--- 7_file/index.js
+++ 7_file/index.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const app = express();
-const port=9000;
 const multer = require("multer");
+const path = require("path"); // 파일 경로를 받았을 때, 그 조작을 도와준다. ex. 확장자를 가져오거나 파일이름을 추출한다.
+const port=9000;
 
 // upload 함수는 multer를 통해 미들웨어로 활용이 가능
 const upload=multer({
     dest: "files/"
 });
+
+// 위의 미들웨어와의 차이점은?
+const uploadDetails=multer({
+    storage: multer.diskStorage({
+        destination: function(req, file, done){
+            done(null, "files/");
+        },
+        filename: function(req, file, done){
+            console.log(file)
+            const ext = path.extname(file.originalname); // 파일의 확장자 이름
+            const basename = path.basename(file.originalname, ext); // 파일의 원래 이름
+            const fileName = basename+"_"+Date.now()+ext;
+            done(null, fileName);
+        }
+    }),
+    limits: {fileSize: 5*1024*1024} // 5mb 제한
+})
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({extended:true}));
@@ -16,10 +34,12 @@ app.get("/", function(req, res){
     res.render("index.ejs");
 })
 
-// single() is one of the middleware. 
+
+// file middleware. single() {req.file(), upload only one file}, array(), fields() {req.files(), upload many files}
 // the file, which `name` is `userfile`, save a file by multer's settings and create 'req.file' object and send to next function.
 // multer create name through uuid. (even omit file type)
-app.post("/upload", upload.single("userfile"), function(req, res){
+// unlike basic set of multer, uploadDetails function make us happy
+app.post("/upload", uploadDetails.single("userfile"), function(req, res){
     console.log("file: ", req.file); // file uploaded through here
     console.log("body: ", req.body); // rest
     res.send("file uploaded!");
